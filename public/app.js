0d2e3991b05175439bd83deaf8b958c3309ef215
indrysfa <indrysfa@gmail.com>
2023-07-18 16:25:22 +0700

    get value form & api fetch

--- public/app.js
+++ public/app.js
@@ -183,36 +183,233 @@ const root = document.querySelector('#root');
 // ReactDOM.render(<App2 />, root);
 
 // Component Lifecycle
-function App3() {
-  const [diklik, setDiklik] = React.useState(false);
-  const [count, setCount] = React.useState(0);
-  // component did update
-  // function akan dieksekusi pada saat component itu di render
-  // ketika nilainya berubah maka function ini akan dieksekusi lagi sesuai dependency state
-  // tidak hanya di render saat pertama kali, ketika render ulang function ini akan di eksekusi lagi
+// function App3() {
+//     const [diklik, setDiklik] = React.useState(false);
+//     const [count, setCount] = React.useState(0);
+//     // component did update
+//     // function akan dieksekusi pada saat component itu di render
+//     // ketika nilainya berubah maka function ini akan dieksekusi lagi sesuai dependency state
+//     // tidak hanya di render saat pertama kali, ketika render ulang function ini akan di eksekusi lagi
+
+//     // component did mount
+//     // React.useEffect(
+//     //     function () {
+//     //         console.log('exec');
+//     //     }, []); // empty array, second argument eksekusi func ini pada saat component itu di render pertama kali saja (data patching dari server)
+
+//     // React.useEffect(
+//     //     function () {
+//     //         console.log(document.getElementById('judul'));
+//     //     },
+//     //     [diklik, count] // state yg mau dipantau
+//     // );
+
+//     React.useEffect(function () {
+//         console.log('init carousel');
+
+//         return function () {
+//             console.log('destroy carousel');
+//         };
+//     }, []);
+
+//     return (
+//         <>
+//             <h1 id="judul">Hello ini judul</h1>;
+//             <button onClick={function () {
+//                 setDiklik(true);
+//             }}>
+//                 Klik disini
+//             </button>
+//             <button onClick={function () {
+//                 setCount(count + 1);
+//             }}>
+//                 Tambah
+//             </button>
+//             <h3>Nilai saat ini: {count}</h3>
+//         </>
+//     )
+// }
+
+// setTimeout(function () {
+//     ReactDOM.render(<App3 />, root);
+// }, 1000);
+
+// Conditional Rendering
+// function App4() {
+//     const [login, setLogin] = React.useState(false);
+
+//     if (login) {
+//         return (
+//             <>
+//                 <h1>Login Success</h1>
+//                 <button onClick={function () {
+//                     setLogin(false);
+//                 }}>
+//                     Logout
+//                 </button>
+//             </>
+//         );
+//     }
+
+//     return (
+//         <>
+//             <h1>Login Page</h1>
+//             <button onClick={function () {
+//                 setLogin(true);
+//             }}>
+//                 Login
+//             </button>
+//         </>
+//     );
+// }
+
+// ReactDOM.render(<App4 />, root);
+
+// function App5() {
+//     const [login, setLogin] = React.useState(false);
+
+//     return (
+//         <>
+//             <h1>Suss App</h1>
+//             <p>{login ? <b>Terima kasih kamu sudah login</b> : <i>Kamu belum login</i>}</p>
+//             <button onClick={function () {
+//                 setLogin(true);
+//             }}>
+//                 Login
+//             </button>
+//         </>
+//     );
+// }
+
+// ReactDOM.render(<App5 />, root);
+
+// Manipulation DOM
+// function App6() {
+//     const [login, setLogin] = React.useState(false); // re render component itu
+//     const judulRef = React.useRef(null); // ketika value nya berubah tidak melakukan re render
+
+//     React.useEffect(function () {
+//         const judul = document.getElementById('jdapp');
+//         setTimeout(function () {
+//             judul.textContent = 'Aplication 1';
+//         }, 1000);
+//     }, []);
+
+//     React.useEffect(function () {
+//         setTimeout(function () {
+//             judulRef.current.textContent = 'Aplication 2';
+//         }, 1000);
+//     }, []);
+
+//     return (
+//         <>
+//             <h1 id="jdapp">Suss App 1</h1>
+//             <h1 ref={judulRef}>Suss App 2</h1>
+//         </>
+//     );
+// }
+
+// ReactDOM.render(<App6 />, root);
+
+// List
+// function App7() {
+//     const fruits = ['Apple', 'Orange', 'Grape', 'Banana'];
+
+//     return (
+//         <ul>
+//             {fruits.map(function (fruit) {
+//                 return <li key={fruit}>{fruit}</li>; // tambahankan key untuk unique value
+//             })}
+//         </ul>
+//     );
+// }
+
+// ReactDOM.render(<App7 />, root);
+
+// Get Value from input form
+// function App8() {
+//     const namaRef = React.useRef(null);
+
+//     function whenSubmit(event) {
+//         event.preventDefault();
+//         const nama = namaRef.current.value;
+
+//         console.log('Nama: ', nama);
+//     }
+
+//     return (
+//         <form onSubmit={whenSubmit}>
+//             <div>
+//                 <label>Name: </label>
+//                 <input type="text" name="nama" ref={namaRef} />
+//             </div>
+//             <button type="submit">Kirim</button>
+//         </form>
+//     );
+// }
+
+// ReactDOM.render(<App8 />, root);
+
+// set Value Default #11
+// function App9() {
+//     const [nama, setNama] = React.useState('Indry');
+
+//     function whenSubmit1(event) {
+//         event.preventDefault();
+
+//         console.log('Nama: ', nama);
+//     }
+
+//     return (
+//         <form onSubmit={whenSubmit1}>
+//             <div>
+//                 <label>Name: </label>
+//                 <input
+//                     type="text"
+//                     name="nama"
+//                     value={nama}
+//                     onChange={function (event) {
+//                         setNama(event.target.value);
+//                     }}
+//                 />
+//             </div>
+//             <button type="submit">Kirim</button>
+//         </form>
+//     );
+// }
+
+// ReactDOM.render(<App9 />, root);
+
+// Get data fecth from API 1 (then untuk dapet sinyal bahwa dia udah selesai hasil promise)
+// function App10() {
+//     React.useEffect(function () {
+//         const getData = fetch('https://api.spaceflightnewsapi.net/v4/blogs/')
+//             .then(function (response) {
+//                 return response.json();
+//             })
+//             .then(function (response) {
+//                 console.log(response);
+//             });
+//         console.log(getData);
+//     }, []);
+
+//     return <h1>Data Fetch (check console log)</h1>;
+// }
+
+// ReactDOM.render(<App10 />, root);
+
+// Get data fecth from API 2 (nunggu hasil promise) #22
+function App11() {
   React.useEffect(function () {
-    console.log(document.getElementById('judul'));
-  }, [count] // state yg mau dipantau
-  );
-
-  // component did mount
-  // React.useEffect(
-  //     function () {
-  //         console.log('exec');
-  //     }, []); // empty array, second argument eksekusi func ini pada saat component itu di render pertama kali saja (data patching dari server)
-
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
-    id: "judul"
-  }, "Hello ini judul"), ";", /*#__PURE__*/React.createElement("button", {
-    onClick: function () {
-      setDiklik(true);
+    async function getData1() {
+      // data diproses dulu
+      const request = await fetch('https://api.spaceflightnewsapi.net/v4/blogs/');
+      const response = await request.json(); // ini juga diproses
+      console.log(response); // lalu di eksekusi
     }
-  }, "Klik disini"), /*#__PURE__*/React.createElement("button", {
-    onClick: function () {
-      setDiklik(true);
-    }
-  }, "Klik disini"), "Nilai saat ini: ", count);
+
+    getData1();
+  }, []);
+  return /*#__PURE__*/React.createElement("h1", null, "Data Fetch (check console log)");
 }
-setTimeout(function () {
-  ReactDOM.render( /*#__PURE__*/React.createElement(App3, null), root);
-}, 1000);
+ReactDOM.render( /*#__PURE__*/React.createElement(App11, null), root);
